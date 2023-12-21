4b628d0102a4829c0f2484fa3f781482a60ccaca
Benjamin <onyedikachi1997@gmail.com>
2023-12-21 11:31:31 +0100

    Added tests.

--- src/emails/accounts.js
+++ src/emails/accounts.js
@@ -1,29 +1,29 @@
-const formData = require('form-data');
-const Mailgun = require('mailgun.js');
-const sgMail = require('@sendgrid/mail')
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+// const formData = require('form-data');
+// const Mailgun = require('mailgun.js');
+// const sgMail = require('@sendgrid/mail')
+// // sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 
 
-const sendWelcomeEmail = (email, name) =>{
-    sgMail.send({
-        to: email,
-        from: 'benjamin@example.com',
-        subject: 'Test Mail',
-        text: `Welcome to the app, ${name}. We hope to make your life easier.`
-    })
-}
+// const sendWelcomeEmail = (email, name) =>{
+//     sgMail.send({
+//         to: email,
+//         from: 'benjamin@example.com',
+//         subject: 'Test Mail',
+//         text: `Welcome to the app, ${name}. We hope to make your life easier.`
+//     })
+// }
 
-const sendDeleteEmail = (email, name) =>{
-    sgMail.send({
-        to: email,
-        from: 'benjamin@example.com',
-        subject: 'Sad to see you go',
-        text: `Hello, ${name}. We are sad to see you go. Is there anything we could have do better?`
-    })
-}
+// const sendDeleteEmail = (email, name) =>{
+//     sgMail.send({
+//         to: email,
+//         from: 'benjamin@example.com',
+//         subject: 'Sad to see you go',
+//         text: `Hello, ${name}. We are sad to see you go. Is there anything we could have do better?`
+//     })
+// }
 
-module.exports = {
-    sendWelcomeEmail,
-    sendDeleteEmail
-}
+// module.exports = {
+//     sendWelcomeEmail,
+//     sendDeleteEmail
+// }
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const multer = require('multer')
-const sharp = require('sharp')
+// const multer = require('multer')
+// const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
-const {sendWelcomeEmail, sendDeleteEmail} = require('../emails/accounts')
+// const {sendWelcomeEmail, sendDeleteEmail} = require('../emails/accounts')
 const router = express.Router()
 
 router.post('/users', async (req, res) => {
@@ -106,46 +106,46 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 })
 
-const upload = multer({
-    // dest: 'avatars',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb){
-        if (!file.originalname.match(/\.(jpg|png|jpeg)/)){
-            return cb(new Error('Please upload an image file'))
-        }
-        cb(undefined, true)
-    }
-})
+// const upload = multer({
+//     // dest: 'avatars',
+//     limits: {
+//         fileSize: 1000000
+//     },
+//     fileFilter(req, file, cb){
+//         if (!file.originalname.match(/\.(jpg|png|jpeg)/)){
+//             return cb(new Error('Please upload an image file'))
+//         }
+//         cb(undefined, true)
+//     }
+// })
 
 
-router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) =>{
-    const buffer  = await sharp(req.file.buffer).resize({width:250, height:250}).png().toBuffer()
-    req.user.avatar = buffer
-    await req.user.save()
-    res.send()
-}, (error, req, res, next) =>{
-    res.status(400).send({error: error.message})
-})
+// router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) =>{
+//     const buffer  = await sharp(req.file.buffer).resize({width:250, height:250}).png().toBuffer()
+//     req.user.avatar = buffer
+//     await req.user.save()
+//     res.send()
+// }, (error, req, res, next) =>{
+//     res.status(400).send({error: error.message})
+// })
 
-router.delete('/users/me/avatar', auth, async (req, res) =>{
-    req.user.avatar = undefined
-    await req.user.save()
-    res.send()
-})
+// router.delete('/users/me/avatar', auth, async (req, res) =>{
+//     req.user.avatar = undefined
+//     await req.user.save()
+//     res.send()
+// })
 
-router.get('/users/:id/avatar', async (req, res) =>{
-    try{
-        const user = await User.findById(req.params.id)
-        if (!user || !user.avatar){
-            throw new Error()
-        }
-        res.set('Content-Type', 'image/png')
-        res.send(user.avatar)
-    } catch (e){
-        res.status(404).send()
-    }
-})
+// router.get('/users/:id/avatar', async (req, res) =>{
+//     try{
+//         const user = await User.findById(req.params.id)
+//         if (!user || !user.avatar){
+//             throw new Error()
+//         }
+//         res.set('Content-Type', 'image/png')
+//         res.send(user.avatar)
+//     } catch (e){
+//         res.status(404).send()
+//     }
+// })
 
 module.exports = router
